650c531a1e7a4b4c13c8e298aaf195b1dc59c217
Carlos Andrey Henao Rincón <carlos.henao@f2x.com.co>
2026-04-09 02:31:00 -0500

    Ajustes para automatización

--- src/main/java/com/pruebareservas/controller/UsuarioController.java
+++ src/main/java/com/pruebareservas/controller/UsuarioController.java
@@ -54,7 +54,7 @@ public class UsuarioController {
 
             ApiResponseDTO<UsuarioDTO> response = ApiResponseDTO.<UsuarioDTO>builder()
                     .statusCode(200)
-                    .message("Autenticación exitosa")
+                    .message("Autenticación realizada exitosamente")
                     .data(usuario)
                     .success(true)
                     .build();
